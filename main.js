1a3d1416df5bd1ddc9fa6bce0d006ff4943665d2
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-02-19 18:00:58 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ var x = 16, y = 20;
 var frames = 0;
 
 function init() {
-    mode(0);
+    mode(1);
 }
 function update() {
     pset(x,y);
+    circ(10,30, 5);
 
     if (btn(0)) {x -= 0.5;}
     if (btn(1)) x += 0.5;
@@ -14,7 +15,9 @@ function update() {
     if (btn(2)) y -= 0.5;
     if (btn(3)) y += 0.5;
 
-    if (btn(0) && frames > 15) { sound(200,200); frames = 0}
+    if (btn(0) && frames > 15) { sound(493,100); frames = 0}
+
+    text(60, 40,40)
 
     frames += 1;
 }
