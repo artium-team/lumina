f647938ab28c43d5a5170eab73507805ca1a65c0
Nguyễn Gia Bách <nguyengiabach1201@gmail.com>
2024-02-20 13:59:24 +0700

    Add files via upload

--- src/lumina.js
+++ src/lumina.js
@@ -40,7 +40,7 @@
         ctx = canvas.getContext("2d");
 
         const style = document.createElement("style");
-        style.innerHTML = "body { margin: 0; overscroll-behavior-y: contain; height: 100%; overflow: hidden } #lumina-canvas {background-color: black; image-rendering: pixelated;} .button,.dpad-button{font-size:20px;text-align:center;line-height:50px;cursor:pointer}#canvas-container{display:flex;flex-direction:column;align-items:center;justify-content:center;width:100vw;height:100vh}#lumina-canvas{border:1px solid #000;}.button-container{display:flex;gap:20px;padding:22px;background-color: #ffbf00;border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;}.button{width:50px;height:50px;border:1px solid #000;border-radius:5px}.dpad-container{display:flex;flex-direction:column;align-items:center;justify-content:center;position:relative;width:150px;height:150px}.dpad-button{position:absolute;width:50px;height:50px;border:1px solid #000;border-radius:50%}.button:active,.dpad-button:active{background:azure}.left,.right{top:50%;transform:translateY(-50%)}.left{left:0}.right{right:0}.down,.up{left:50%;transform:translateX(-50%)}.up{top:0}.down{bottom:0}";
+        style.innerHTML = "body { margin: 0; overscroll-behavior-y: contain; height: 100%; overflow: hidden } #lumina-canvas {background-color: black; image-rendering: pixelated;} .button,.dpad-button{font-size:20px;text-align:center;line-height:50px;cursor:pointer}#canvas-container{display:flex;flex-direction:column;align-items:center;justify-content:center;width:100vw;height:100vh}#lumina-canvas{border:1px solid #000;}.button-container{display:flex;gap:20px;padding:22px;background-color: #ffbf00;border-bottom-left-radius: 25px;border-bottom-right-radius: 25px;}.button{width:50px;height:50px;border:1px solid #000;border-radius:5px}.dpad-container{display:flex;flex-direction:column;align-items:center;justify-content:center;position:relative;width:150px;height:150px}.dpad-button{position:absolute;width:50px;height:50px;border:1px solid #000;border-radius:50%;}.button:active,.dpad-button:active, .active{background:azure; -webkit-tap-highlight-color: transparent; }.left,.right{top:50%;transform:translateY(-50%)}.left{left:0}.right{right:0}.down,.up{left:50%;transform:translateX(-50%)}.up{top:0}.down{bottom:0}";
         document.getElementsByTagName("head")[0].appendChild(style);
 
         function fromHTML(html, trim = true) {
@@ -84,7 +84,6 @@
 
         leftButton.addEventListener("mousedown", () => {
             keysPressed['ArrowLeft'] = true;
-            console.log("!");
             if (notStart) {
                 intro();
                 notStart = false;
@@ -132,6 +131,7 @@
                 intro();
                 notStart = false;
             }
+            leftButton.classList.add("active");
         });
         rightButton.addEventListener("touchstart", () => {
             keysPressed['ArrowRight'] = true;
@@ -139,6 +139,7 @@
                 intro();
                 notStart = false;
             }
+            rightButton.classList.add("active");
         });
         upButton.addEventListener("touchstart", () => {
             keysPressed['ArrowUp'] = true;
@@ -146,6 +147,7 @@
                 intro();
                 notStart = false;
             }
+            upButton.classList.add("active");
         });
         downButton.addEventListener("touchstart", () => {
             keysPressed['ArrowDown'] = true;
@@ -153,6 +155,7 @@
                 intro();
                 notStart = false;
             }
+            downButton.classList.add("active");
         });
         aButton.addEventListener("touchstart", () => {
             keysPressed['z'] = true;
@@ -160,6 +163,7 @@
                 intro();
                 notStart = false;
             }
+            aButton.classList.add("active");
         });
         bButton.addEventListener("touchstart", () => {
             keysPressed['x'] = true;
@@ -167,25 +171,32 @@
                 intro();
                 notStart = false;
             }
+            bButton.classList.add("active");
         });
 
         leftButton.addEventListener("touchend", () => {
             keysPressed['ArrowLeft'] = false;
+            leftButton.classList.remove("active");
         });
         rightButton.addEventListener("touchend", () => {
             keysPressed['ArrowRight'] = false;
+            rightButton.classList.remove("active");
         });
         upButton.addEventListener("touchend", () => {
             keysPressed['ArrowUp'] = false;
+            upButton.classList.remove("active");
         });
         downButton.addEventListener("touchend", () => {
             keysPressed['ArrowDown'] = false;
+            downButton.classList.remove("active");
         });
         aButton.addEventListener("touchend", () => {
             keysPressed['z'] = false;
+            aButton.classList.remove("active");
         });
         bButton.addEventListener("touchend", () => {
             keysPressed['x'] = false;
+            bButton.classList.remove("active");
         });
 
         leftButton.addEventListener("mouseup", () => {
@@ -235,16 +246,18 @@
         font.load().then(function (font) {
             document.fonts.add(font);
             ctx.font = `${8 * resolution}px font`;
+
+            mode(1);
             try {
                 init();
                 cls();
-
-                resolution = 350 / 64;
-                ctx.font = `${8 * resolution}px font`;
-                text("Press any button", 2, 30);
-                text("to start", 17, 38);
-                mode(currentMode);
             } catch { }
+
+            resolution = 350 / 64;
+            ctx.font = `${8 * resolution}px font`;
+            text("Press any button", 2, 30);
+            text("to start", 17, 38);
+            mode(currentMode);
         });
     }
 
@@ -256,11 +269,15 @@
 
         if (numFrames > introFrames == false) {
             numFrames += 5;
-            circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames, 8);
-            circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 20, 9);
-            circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 40, 10);
-            circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 60, 11);
-            circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 60, 0);
+
+            if (!mobileCheck()) {
+                circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames, 8);
+                circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 20, 9);
+                circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 40, 10);
+                circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 60, 11);
+                circfill(Math.pow(2, currentMode) * 32 / 2, Math.pow(2, currentMode) * 32 / 2, numFrames - 60, 0);
+            }
+            
             if (numFrames > 60 && !note1) {
                 sound(622, 200);
                 note1 = true;
@@ -290,6 +307,12 @@
     }
 
     let notStart = true;
+    const mobileCheck = function () {
+        let check = false;
+        (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true; })(navigator.userAgent || navigator.vendor || window.opera);
+        console.log(check);
+        return check;
+    };
     window.onload = () => {
         setup();
     }
